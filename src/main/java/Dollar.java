9928d758ddd8930f050483c3551f74bd1e343ab2
daicham <daicham@gmail.com>
2011-07-27 00:58:06 +0900

    P.29 equals()をPullUp。グリーンバー。

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -7,10 +7,4 @@ class Dollar extends Money {
 	Money times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
-
-	@Override
-	public boolean equals(Object object) {
-		Money dollar = (Money) object;
-		return amount == dollar.amount;
-	}
 }
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,10 @@
 class Money {
 
 	protected int amount;
+
+	@Override
+	public boolean equals(Object object) {
+		Money dollar = (Money) object;
+		return amount == dollar.amount;
+	}
 }
