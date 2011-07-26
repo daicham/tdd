a5f9bcea7cadc98af07865b2398022c12610bc44
daicham <daicham@gmail.com>
2011-07-27 00:54:49 +0900

    P.29 Dollerの親クラスにMoneyを指定して amount を PullUp。グリーンバー。

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,18 +1,16 @@
-class Dollar {
-
-	private int amount;
+class Dollar extends Money {
 
 	Dollar(int amount) {
 		this.amount = amount;
 	}
 
-	Dollar times(int multiplier) {
+	Money times(int multiplier) {
 		return new Dollar(amount * multiplier);
 	}
 
 	@Override
 	public boolean equals(Object object) {
-		Dollar dollar = (Dollar) object;
+		Money dollar = (Money) object;
 		return amount == dollar.amount;
 	}
 }
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,2 +1,4 @@
 class Money {
+
+	protected int amount;
 }
