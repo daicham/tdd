332872f87c423aa2ab1d2d9a8d92bf344ca2e097
daicham <daicham@gmail.com>
2011-07-27 01:09:33 +0900

    P.31 Franc#amount をPullUp、Franc#equals()もPullUp(実際にはすでに存在するので削除)。グリーンバー。

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -1,19 +1,11 @@
-class Franc {
-
-	private int amount;
+class Franc extends Money {
 
 	Franc(int amount) {
 		this.amount = amount;
 	}
 
-	Franc times(int multiplier) {
+	Money times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-
-	@Override
-	public boolean equals(Object object) {
-		Franc dollar = (Franc) object;
-		return amount == dollar.amount;
 	}
 
 }
