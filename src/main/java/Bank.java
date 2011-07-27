e9b8e30148659ee6381e399feb2656d78844b428
daicham <daicham@gmail.com>
2011-07-28 00:08:52 +0900

    P.63 Bank#reduce()のテストコードを追加。とりあえずreduce()を実装した。グリーンバー。

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,7 +1,9 @@
 public class Bank {
 
 	Money reduce(Expression source, String to) {
-		return Money.dollar(10);
+		Sum sum = (Sum) source;
+		int amount = sum.augend.amount + sum.addend.amount;
+		return new Money(amount, to);
 	}
 
 }
