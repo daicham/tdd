8da1f69562ce6c6ad900f1afa83880838cf600d0
daicham <daicham@gmail.com>
2011-07-28 00:32:48 +0900

    P.65 すると、Bank#reduce()がすっきりできる。グリーンバー。

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,11 +1,7 @@
 public class Bank {
 
 	Money reduce(Expression source, String to) {
-		if (source instanceof Money) {
-			return (Money) source.reduce(to);
-		}
-		Sum sum = (Sum) source;
-		return sum.reduce(to);
+		return source.reduce(to);
 	}
 
 }
