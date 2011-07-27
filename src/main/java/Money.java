e26cadd2db49852ec230b340cffe9f5da34588c6
daicham <daicham@gmail.com>
2011-07-27 22:27:16 +0900

    P.48 異なる通貨クラスでの等価性テストを追加し、レッドバー。クラスではなく通貨コードを比較するように変更。グリーンバー。

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -11,7 +11,7 @@ class Money {
 	@Override
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount && getClass().equals(money.getClass());
+		return amount == money.amount && currency().equals(money.currency());
 	}
 
 	Money times(int multiplier) {
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -32,4 +32,9 @@ public class MoneyTest {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 	}
+
+	@Test
+	public void testDifferentClassEquality() throws Exception {
+		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+	}
 }
