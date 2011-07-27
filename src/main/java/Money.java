1f7648019407c7c01ecc0ae69ac3d0a5603f7390
daicham <daicham@gmail.com>
2011-07-27 23:28:18 +0900

    P.58 以下のメタファによりtestSimpleAddition()のテストコードを変更。コンパイルが通り、仮実装までコードを準備した。ここはちと難解。グリーンバー。 - 2つの通貨の合計を表すオブジェクトを作成する。(Expression) - 操作(通貨の計算)の結果(Expression)とする。 - 結果から為替レートにより1つの通貨に変換される。
    今回のテストケースでは $5 + $5 = $10 を実装。
    為替レートの変換はBankクラスが行う。

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression {
 
 	protected int amount;
 	protected String currency;
@@ -35,7 +35,7 @@ class Money {
 		return amount + " " + currency;
 	}
 
-	Money plus(Money addend) {
+	Expression plus(Money addend) {
 		return new Money(amount + addend.amount, currency);
 	}
 
