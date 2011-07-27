126a3b0766b5cea1c372f6d7cbdab7d35322497e
daicham <daicham@gmail.com>
2011-07-28 00:27:06 +0900

    P.64 とりあえず、Bank#reduce()でMoneyの場合の処理とテストコードを追加したがひどい実装。それでもグリーンバー。

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,6 +1,9 @@
 public class Bank {
 
 	Money reduce(Expression source, String to) {
+		if (source instanceof Money) {
+			return (Money) source;
+		}
 		Sum sum = (Sum) source;
 		return sum.reduce(to);
 	}
