7b19b65bc49ce2eb5f6b4726ba82af4a5c50e9bd
daicham <daicham@gmail.com>
2011-10-15 21:07:23 +0900

    P.70 rate 2 が埋め込まれていたためBankでrateを追加できるようにリファクタリング、だがレッドバー

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,15 +1,37 @@
+import java.util.Hashtable;
+
 public class Bank {
+	private Hashtable<Pair, Integer> rates = new Hashtable<Pair, Integer>();
 
 	Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
 
-	public void addRate(String string, String string2, int i) {
-		// TODO Auto-generated method stub
+	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), rate);
 	}
 
 	public int rate(String from, String to) {
-		int rate = from.equals("CHF") && to.equals("USD") ? 2 : 1;
-		return rate;
+		return rates.get(new Pair(from, to));
+	}
+
+	private class Pair {
+		private String from;
+		private String to;
+
+		Pair(String from, String to) {
+			this.from = from;
+			this.to = to;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			Pair pair = (Pair) obj;
+			return from.equals(pair.from) && to.equals(pair.to);
+		}
+		@Override
+		public int hashCode() {
+			return 0;
+		}
 	}
 }
